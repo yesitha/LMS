dbc4e709bd9f0a968a234e6e9e28d9e04cf609bd
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2024-05-21 09:00:14 +0530

    added  get user details util

--- lib-common/src/main/java/com/itgura/util/UserUtil.java
+++ lib-common/src/main/java/com/itgura/util/UserUtil.java
@@ -1,37 +1,72 @@
 package com.itgura.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.itgura.exception.BadRequestRuntimeException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Component
 @Getter
 @Setter
 public class UserUtil {
-    @Autowired
-    private UserStore requestUserStore;
 
-    public UUID getUserId(String jwtToken) throws JsonProcessingException {
-        return getUserId();
+
+    public String getUserEmail(String jwtToken, String SECRET_KEY) throws BadRequestRuntimeException {
+        if (jwtToken != null && jwtToken.startsWith("Bearer ")) {
+            try {
+                String token = jwtToken.substring(7);
+                Claims claims = Jwts.parser()
+                        .setSigningKey(SECRET_KEY) // Replace with your actual secret key
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String email = claims.getSubject();
+
+                return email;
+
+
+            } catch (Exception e) {
+                throw new BadRequestRuntimeException("Invalid Token");
+
+            }
+        }
+
+        return null;
+
     }
-//    public static ThreadLocal<ThreadUserStore> threadUserStore = new ThreadLocal<>();
 
-    public UUID getUserId() throws JsonProcessingException {
-        return requestUserStore.getUserId();
+    public List<String> getUserRoles(String jwtToken, String SECRET_KEY) throws BadRequestRuntimeException {
+        if (jwtToken != null && jwtToken.startsWith("Bearer ")) {
+            try {
+                String token = jwtToken.substring(7);
+                Claims claims = Jwts.parser()
+                        .setSigningKey(SECRET_KEY) // Replace with your actual secret key
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
 
-//        if (null != RequestContextHolder.getRequestAttributes()) {
-//            return requestUserStore.getUserId();
-//        }
-//        else {
-//            try {
-//                return threadUserStore.get().getUserId();
-//            } catch (Exception e) {
-//                return 1l;
-//            }
-//        }
+                List<Map<String, String>> roles = (List<Map<String, String>>) claims.get("roles");
+                List<String> authorities = roles.stream()
+                        .map(roleMap -> roleMap.get("authority"))
+                        .toList();
+
+                return authorities;
+
+
+            } catch (Exception e) {
+                throw new BadRequestRuntimeException("Invalid Token");
+
+            }
+        }
+        return null;
     }
 }
