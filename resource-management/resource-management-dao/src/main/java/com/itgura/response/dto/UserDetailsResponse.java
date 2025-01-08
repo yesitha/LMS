efdebf5c7548b1a30c72035aa39c21d644d3b35a
Savindu Panagoda <86566770+savindu29@users.noreply.github.com>
2025-01-08 20:50:20 +0530

    add student user apis

--- resource-management/resource-management-dao/src/main/java/com/itgura/response/dto/UserDetailsResponse.java
+++ resource-management/resource-management-dao/src/main/java/com/itgura/response/dto/UserDetailsResponse.java
@@ -1,15 +1,13 @@
 package com.itgura.response.dto;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.itgura.request.AddressRequest;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
